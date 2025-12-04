847c5461f7901a8ad183f60ff215df409793d09a
gilangfatahilah <gilangf010@gmail.com>
2025-12-04 12:41:42 +0700

    🐞 fix(utils): update cookie settings for production environment

--- backend/src/lib/utils.ts
+++ backend/src/lib/utils.ts
@@ -11,8 +11,8 @@ export const generateToken = (userId: string, res: Response): string => {
   res.cookie('jwt', token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: 'strict',
-    secure: process.env.NODE_ENV !== "development"
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    secure: process.env.NODE_ENV === "production",
   });
 
   return token;
